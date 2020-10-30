13a84553b0fbf3d6c6088791c93f9dea1825f1a4
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-30 20:43:33 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import wx
+import wx.adv
+
+
+class App(wx.App):
+
+	path = "%userprofile%/appdata/Local/Programs"
+
+	def OnPreInit(self):
+		wx.Exit()
+
+	def OnInit(self):
+		pass
+
+	def OnExit(self):
+		pass
+
+
+if __name__ == '__main__':
+
+	app = App()
+	app.MainLoop()
