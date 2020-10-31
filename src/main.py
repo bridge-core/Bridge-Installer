f2057f2df115b9920bf53682d7678099bcdbe8f9
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-11-01 00:25:36 +0100

    last touches

--- src/main.py
+++ src/main.py
@@ -26,6 +26,8 @@ class App(wx.App):
 	def OnInit(self):
 		if self.instanceChecker.IsAnotherRunning():
 			return False
+		if not self.installPath.exists():
+			self.installPath.mkdir(exist_ok=True, parents=True)
 		self.SetAppName('BridgeInstaller')
 		self.root = root(self)
 		self.root.Show(True)
